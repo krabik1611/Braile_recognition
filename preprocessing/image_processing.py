c57ba5edc1f421c7d3d1dc8793df9ca2950f79ee
krabik1611 <danilmelnikov11@gmail.com>
2020-07-05 18:23:04 +0500

    add comments

--- preprocessing/image_processing.py
+++ preprocessing/image_processing.py
@@ -2,41 +2,44 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_path = "test/1.jpg"
 
 class ImageProcessing():
+	'''class for image processing'''
 	def __init__(self,file_path):
+		'''define file path and initialize proprities'''
 		self.file_path = file_path
-		self.images = []
-		self.titles = ['Original Image', 'Global Thresholding (v = 127)',
-						'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-		self.imageProcessing()
+		self.imageProcessing() #start processing image after create object
 		
 	def imageProcessing(self):
-		img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+		'''main processing function'''
+		img = cv.imread(img_path, cv.IMREAD_GRAYSCALE) #read image
 		img = cv.medianBlur(img,3)
-		ret,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
-		th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
-					cv.THRESH_BINARY,11,2)
 		th3 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
 					cv.THRESH_BINARY,11,2)
 		
-		self.images = [img, th1, th2, th3]
+		self.images =  [img,th3]
 		self.mainImage = th3
 	def showPlot(self):
-		
-		for i in range(4):
-			plt.subplot(2,2,i+1)
+		'''visualization changes'''
+		titles = ['Original Image', 'Adaptive Gaussian Thresholding'] #titles for plot
+		for i in range(2):
+			plt.subplot(1,2,i+1)
 			plt.imshow(self.images[i],'gray')
-			plt.title(self.titles[i])
+			plt.title(titles[i])
 			plt.xticks([]),plt.yticks([])
 
 		plt.show()
+		
 	def getGaussianImage(self):
+		'''get processing image'''
 		return self.mainImage
+		
 	def saveGaussianImage(self,save_path):
+		'''save image'''
 		plt.imshow(self.mainImage)
 		cv.imwrite(save_path,self.mainImage)
-		
+
+img_path = "test/1.jpg"
+
 obj = ImageProcessing(img_path)
 obj.showPlot()
