d6dcd5811ab775f8ef3159bbef962b271ddd4f3b
krabby1611 <danilmelnikov11@gmail.com>
2020-07-07 13:06:56 +0500

    up image processing thx deepkerch<3

--- preprocessing/image_processing.py
+++ preprocessing/image_processing.py
@@ -14,21 +14,33 @@ class ImageProcessing():
 	def imageProcessing(self):
 		'''main processing function'''
 		self.oImage = cv2.imread(self.file_path, cv2.IMREAD_GRAYSCALE) #read image
-		# self.Image = cv2.medianBlur(self.Image,3)
+		self.oImage = cv2.GaussianBlur(self.oImage, (5, 5), 0) # delete trash
 
 	def test(self):
+		'''func for test launch'''
+		self.upgradeImage()
 		self.showImage()
 	def upgradeImage(self):
-		self.Image = cv2.adaptiveThreshold(self.oImage,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-					cv2.THRESH_BINARY,11,2)
-		# self.upImage = morphology.remove_small_holes(np.array(self.Image,bool),10)
-	def showImage(self,image=0):
-		if image==0:
-			image = self.oImage
-		plt.subplot(1,2,1),plt.imshow(self.oImage)
+		'''upgrage image 2 times'''
+		self.Image = cv2.Canny(self.oImage, 20, 50)
+		self.inv()
+
+	def inv(self):
+		'''make inverse pic'''
+		_,self.wImage = cv2.threshold(self.Image,127,255,cv2.THRESH_BINARY_INV)
+
+
+	def showImage(self):
+		'''show changes in one layout'''
+		orig = cv2.cvtColor(self.oImage,cv2.COLOR_BGR2RGB)
+		mod = cv2.cvtColor(self.Image, cv2.COLOR_BGR2RGB)
+		mod2 = self.wImage #cv2.cvtColor(self.wImage,cv2.COLOR_BGR2RGB)
+		plt.subplot(1,3,1),plt.imshow(orig)
 		plt.title('Orig'), plt.xticks([]),plt.yticks([])
-		plt.subplot(1,2,2),plt.imshow(image)
-		plt.title('test'), plt.xticks([]),plt.yticks([])
+		plt.subplot(1,3,2),plt.imshow(mod)
+		plt.title('mod1'), plt.xticks([]),plt.yticks([])
+		plt.subplot(1,3,3),plt.imshow(mod2,'gray')
+		plt.title('mod2'), plt.xticks([]),plt.yticks([])
 		plt.show()
 image = ImageProcessing('../dataFiles/origImage/1.jpg')
 image.test()
